0c0c776f0281f9f40cf10cc7464e10ee24d63b11
Xuân Quý <81242756+xqsadness@users.noreply.github.com>
2024-07-26 14:38:23 +0700

    init

---
+++ TaskManagement/TaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApp.swift
+//  TaskManagement
+//
+//  Created by xqsadness on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
